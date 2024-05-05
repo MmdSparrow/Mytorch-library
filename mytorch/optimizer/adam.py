89d3c316ae02804817d6c6b3326982242cee63d2
MmdSparrow <esmirk.137@gmail.com>
2024-05-05 20:42:56 +0330

    adam implemented and work correctly!

--- mytorch/optimizer/adam.py
+++ mytorch/optimizer/adam.py
@@ -18,20 +18,36 @@ class Adam(Optimizer):
         self.beta2 = beta2
         self.epsilon = epsilon
         self.time = time
-        self.momentums = [np.zeros_like(param) for layer in self.layers for param in layer.parameters()]
-        self.velocities = [np.zeros_like(param) for layer in self.layers for param in layer.parameters()]
+        self.momentums = {}
+        self.velocities = {}
+
+        for layer_index, layer in enumerate(self.layers):
+            self.momentums[(layer_index, "w")] = Tensor(np.zeros_like(layer.weight.data))
+            self.velocities[(layer_index, "w")] = Tensor(np.zeros_like(layer.weight.data))
+            if layer.need_bias:
+                self.momentums[(layer_index, "b")] = Tensor(np.zeros_like(layer.bias.data))
+                self.velocities[(layer_index, "b")] = Tensor(np.zeros_like(layer.bias.data))
 
     def step(self):
         self.time += 1
         beta1_t = self.beta1 ** self.time
         beta2_t = self.beta2 ** self.time
 
+        layer_index= -1
         for layer in self.layers:
-            for param_idx, param in enumerate(layer.parameters()):
-                param_grad = layer.grads[param_idx]
-                self.momentums[param_idx] = self.beta1 * self.momentums[param_idx] + (1 - self.beta1) * param_grad
-                self.velocities[param_idx] = self.beta2 * self.velocities[param_idx] + (1 - self.beta2) * (param_grad ** 2)
-                m_hat = self.momentums[param_idx] / (1 - beta1_t)
-                v_hat = self.velocities[param_idx] / (1 - beta2_t)
-                update = self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
-                layer.parameters()[param_idx] -= update
+            layer_index+=1
+            if layer.weight is not None and layer.weight.requires_grad:
+                self.momentums[(layer_index, "w")] = self.beta1 * self.momentums[(layer_index, "w")] + (1 - self.beta1) * layer.weight.grad
+                self.velocities[(layer_index, "w")] = self.beta2 * self.velocities[(layer_index, "w")] + (1 - self.beta2) * (layer.weight.grad ** 2)
+                m_hat = self.momentums[(layer_index, "w")] * (1/(1 - beta1_t))
+                v_hat = self.velocities[(layer_index, "w")] * (1/(1 - beta2_t))
+                update = self.learning_rate * m_hat * ((v_hat).__pow__(1/2) + self.epsilon).__pow__(-1)
+                layer.weight = layer.weight - update
+
+            if layer.need_bias and layer.bias is not None and layer.bias.requires_grad:
+                self.momentums[(layer_index, "b")] = self.beta1 * self.momentums[(layer_index, "b")] + (1 - self.beta1) * layer.bias.grad
+                self.velocities[(layer_index, "b")] = self.beta2 * self.velocities[(layer_index, "b")] + (1 - self.beta2) * (layer.bias.grad ** 2)
+                m_hat = self.momentums[(layer_index, "b")] * (1/(1 - beta1_t))
+                v_hat = self.velocities[(layer_index, "b")] * (1/(1 - beta2_t))
+                update = self.learning_rate * m_hat * ((v_hat).__pow__(1/2)+ self.epsilon).__pow__(-1)
+                layer.bias = layer.bias - update

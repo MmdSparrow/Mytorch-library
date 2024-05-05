1977d64d03744129c24768a60170bea3f679ebaf
MmdSparrow <esmirk.137@gmail.com>
2024-05-05 19:49:54 +0330

    momentum implemented and work correct!

--- mytorch/optimizer/momentum.py
+++ mytorch/optimizer/momentum.py
@@ -1,3 +1,4 @@
+from torch import Tensor
 from mytorch.optimizer import Optimizer
 import numpy as np
 
@@ -8,16 +9,36 @@ class Momentum(Optimizer):
         self.learning_rate = learning_rate
         self.momentum = momentum
         self.velocities = {}
+        for layer_index, layer in enumerate(self.layers):
+            self.velocities[(layer_index, "w")] = Tensor(np.zeros_like(layer.weight.data))
+            if layer.need_bias:
+                self.velocities[(layer_index, "b")] = Tensor(np.zeros_like(layer.bias.data))
 
     def step(self):
+        layer_index= -1
         for layer in self.layers:
-            for param_name, param in layer.parameters.items():
-                if param_name not in self.velocities:
-                    self.velocities[param_name] = np.zeros_like(param.data)
+            layer_index+=1
+            if layer.weight is not None and layer.weight.requires_grad:
+                # if (layer_index, param_index) not in self.velocities:
+                    # self.velocities[(layer_index, param_index)] = Tensor(np.zeros_like(param.data))
 
-                if param.grad is not None:
-                    self.velocities[param_name] = (
-                            self.momentum * self.velocities[param_name] +
-                            self.learning_rate * param.grad.data
+                if layer.weight.grad is not None:
+                    self.velocities[(layer_index, "w")] = (
+                        # self.velocities[(layer_index, param_index, "w")].__mul__(Tensor([self.momentum])).__add__(Tensor([self.learning_rate]).__mul__(param.grad))
+                        self.momentum * self.velocities[(layer_index, "w")] +
+                        self.learning_rate * layer.weight.grad
                     )
-                    param.data -= self.velocities[param_name]
+                    layer.weight = layer.weight - self.velocities[(layer_index, "w")]
+                        
+            if layer.need_bias and layer.bias is not None and layer.bias.requires_grad:
+                # if (layer_index, param_index) not in self.velocities:
+                    # self.velocities[(layer_index, param_index)] = Tensor(np.zeros_like(param.data))
+
+                if layer.bias.grad is not None:
+                    self.velocities[(layer_index, "b")] = (
+                            self.momentum * self.velocities[(layer_index, "b")] +
+                            self.learning_rate * layer.bias.grad
+                    )
+                    layer.bias = layer.bias - self.velocities[(layer_index, "b")]
+            
+            
